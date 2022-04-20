46fca22d2bff1e833b3f6e0434fe147507e714a2
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-19 21:22:45 -0700

    fixed code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,15 +19,18 @@ void bubble_sort(int *array, size_t size)
 
 	arrayB = array;
 
-	for (x = 1; x < n; x + 1)
+	for (x = 1; x < n; x++)
 	{
-		for (y = 0; y < n - 1; y + 1)
+		for (y = 0; y < n - 1; y++)
 		{
 			if (arrayB[y] > array[y + 1])
 			{
 				temp = arrayB[y + 1];
-				array[y] = array[y + 1];
+				arrayB[y] = array[y + 1];
+				arrayB[y + 1] = temp;
+				print_array(arrayB, n);
 			}
+
 		}
 	}
 }
