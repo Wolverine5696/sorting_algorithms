a684a7f77bd8dc1181cddff212c2e22fcefff2ec
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-19 21:13:50 -0700

    fixed loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+i#include <stdio.h>
 #include "sort.h"
 #include <stdlib>
 
@@ -15,17 +15,17 @@ void bubble_sort(int *array, size_t size)
 	size_t x, y;
 	size_t n = size;
 	int temp = 0;
-	int *array = malloc(n * sizeof(int));
+	int *arrayB = malloc(n * sizeof(int));
 
-	array = array;
+	arrayB = array;
 
-	for (x = 1; x < k; x + 1)
+	for (x = 1; x < n; x + 1)
 	{
-		for (y = 0; y < k - 1; y + 1)
+		for (y = 0; y < n - 1; y + 1)
 		{
-			if (array[y] > array[y + 1])
+			if (arrayB[y] > array[y + 1])
 			{
-				temp = array[y + 1];
+				temp = arrayB[y + 1];
 				array[y] = array[y + 1];
 			}
 		}
